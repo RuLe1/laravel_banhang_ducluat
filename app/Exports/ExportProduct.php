c1776eaf8095dc383ff9eaef21be8f9de3e7c66f
holuat <35761097+RuLe1@users.noreply.github.com>
2022-01-16 16:46:59 +0700

    add title file xuất Excel

--- app/Exports/ExportProduct.php
+++ app/Exports/ExportProduct.php
@@ -4,14 +4,37 @@ namespace App\Exports;
 
 use App\Models\Product;
 use Maatwebsite\Excel\Concerns\FromCollection;
-
-class ExportProduct implements FromCollection
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Events\AfterSheet;
+class ExportProduct implements FromCollection, WithHeadings, ShouldAutoSize
 {
     /**
     * @return \Illuminate\Support\Collection
     */
     public function collection()
     {
-        return Product::all();
+        return Product::query()->select('id',
+                                        'category_id',
+                                        'brand_id',
+                                        'product_name',
+                                        'product_quantity',
+                                        'product_price',
+                                        'product_image',
+                                        'status')->get();
     }
+    public function headings(): array
+    {
+        return [
+            'id',
+            'category_id',
+            'brand_id',
+            'product_name',
+            'product_quantity',
+            'product_price',
+            'product_image',
+            'status'
+        ];
+    } 
 }
